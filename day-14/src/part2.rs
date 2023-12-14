99d6e0689104fcdceae141ed9e43b8b3cd83394a
kenyontu <kenyontu@outlook.com>
2023-12-14 18:22:12 +0800

    day-14 add a pattern detection

--- day-14/src/part2.rs
+++ day-14/src/part2.rs
@@ -3,13 +3,13 @@ struct Grid {
 }
 
 impl Grid {
-    fn calc_north_load(&self) -> u64 {
-        let mut load: u64 = 0;
+    fn calc_north_load(&self) -> u32 {
+        let mut load: u32 = 0;
 
         for i in 0..self.grid.len() {
             for j in 0..self.grid[i].len() {
                 if self.grid[i][j] == 'O' {
-                    let add = (self.grid.len() - i) as u64;
+                    let add = (self.grid.len() - i) as u32;
                     load += add;
                 }
             }
@@ -50,20 +50,46 @@ impl Grid {
     }
 }
 
-pub fn solve(input: &str) -> u64 {
+// The idea is to take the last 3 items and walk backwards until we find
+// the same trio, then by calculating the difference of the position of the
+// original trio we can figure out the pattern and the position it starts.
+pub fn find_pattern(nums: &[u32]) -> Option<(&[u32], usize)> {
+    let last_three = &nums[nums.len() - 3..];
+    let prev_match_index = nums.len() - 3;
+
+    for i in (0..nums.len() - 6).rev() {
+        if &nums[i..i + 3] == last_three {
+            let pattern_len = prev_match_index.abs_diff(i);
+
+            // Making sure there's a previous repeating pattern
+            if &nums[i..prev_match_index] == &nums[i - pattern_len..i] {
+                return Some((&nums[i - pattern_len..i], i - pattern_len));
+            }
+        }
+    }
+
+    None
+}
+
+pub fn solve(input: &str) -> u32 {
     let mut grid = Grid {
         grid: input.lines().map(|l| l.chars().collect()).collect(),
     };
 
-    // Not sure if I was just lucky with the input, but I got the
-    // correct answer with only 1000 cycles
-    let cycles = 1000;
+    let mut loads: Vec<u32> = Vec::new();
+    let cycles = 500;
     for _ in 0..cycles {
         grid.roll_clockwise();
         grid.roll_clockwise();
         grid.roll_clockwise();
         grid.roll_clockwise();
+        loads.push(grid.calc_north_load());
     }
 
-    grid.calc_north_load()
+    if let Some((pattern, index)) = find_pattern(&loads) {
+        let res_index = (1_000_000_000 - index) % pattern.len() - 1;
+        return pattern[res_index];
+    }
+
+    unreachable!();
 }
